f8f9cd06af2ab0de0e2cf13415019b87617f3ab1
tokatu4561 <ryobsk4561@gmail.com>
2022-11-06 23:46:05 +0900

    diディレクトリ整理

--- cmd/getTodos/main.go
+++ cmd/getTodos/main.go
@@ -5,26 +5,16 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/tokatu4561/tasks/pkg/application"
+	"github.com/tokatu4561/tasks/pkg/application/di"
 	"github.com/tokatu4561/tasks/pkg/domain"
-	"github.com/tokatu4561/tasks/pkg/infrastructure/dynamo"
-	"github.com/tokatu4561/tasks/pkg/usecases"
 )
 
 type Response struct {
 	Tasks []*domain.Task `json: "tasks"`
 }
 
-func NewTaskController() *application.TaskController {
-	db := dynamo.NewDynamoDatabaseHandler()
-	taskRepositoryInterface := dynamo.NewTaskRepository(db)
-	taskUseCaseInterface := usecases.NewTaskUsecase(taskRepositoryInterface)
-	taskController := application.NewTaskController(taskUseCaseInterface)
-	return taskController
-}
-
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	ctl := NewTaskController()
+	ctl := di.NewTaskController()
 
 	tasks, err := ctl.GetTasks(request)
 	if err != nil {
--- pkg/application/di/wire_gen.go
+++ pkg/application/di/wire_gen.go
@@ -4,7 +4,7 @@
 //go:build !wireinject
 // +build !wireinject
 
-package main
+package di
 
 import (
 	"github.com/tokatu4561/tasks/pkg/application"
